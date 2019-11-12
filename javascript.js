eb50db34f9258373ea2a617f0fa864d55ffddf8f
555hamza555 <54897450+555hamza555@users.noreply.github.com>
2019-11-12 10:34:04 -0500

    ye

---
+++ javascript.js
@@ -0,0 +1,30 @@
+function third_angle(){
+	let angle_one = parseInt(prompt("What is the first angle?")); 
+	let angle_two = parseInt(prompt("What is the secound angle?"));
+	let angle_three = 180-(angle_one + angle_two);
+	alert ("The 3rd angle is " + angle_three );
+}
+//Math.floor, modulus %
+// function century(){
+// centuryFromYear(2103)  returns (22)
+// centuryFromYear(1865)  returns (19)
+// centuryFromYear(1965)  returns (20)
+// centuryFromYear(2019)  returns (21) 
+// }
+// if, else if, else if, else, for loop
+function four_ten(){
+	for(let i = 0; i < 101; i++ ){
+		 if(i % 10 == 0 && i % 4 == 0){
+			console.log ("four_ten")
+		}
+		else if(i % 10 == 0){
+			console.log ("Ten")
+		}
+		else if (i % 4 == 0) {
+			console.log ("Four")
+		} else {
+			console.log (i)
+		}
+	}
+}
+
